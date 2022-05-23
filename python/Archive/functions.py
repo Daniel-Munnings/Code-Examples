1da719853dd38c82355dd92b79fcc6d888626960
Daniel-Munnings <103251854+Daniel-Munnings@users.noreply.github.com>
2022-05-23 17:34:23 +0100

    Delete python directory

--- python/Archive/functions.py
+++
@@ -1,8 +0,0 @@
-def helloWord(dob):
-    #print("Hello World!")
-    #print(dateToday)
-    if dob == "09/05/1983":
-        print("It's your Birthday!")
-
-def helloWorld(name,age):
-    print(name + " says Hello! and is " + age + " years old")
